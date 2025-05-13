6d141504bfcfec889e7fcee063f4508bfcb17f3b
mybb520 <48597962@qq.com>
2025-05-13 09:43:32 +0000

    更新 SrcJuPublic.js

--- SrcJuPublic.js
+++ SrcJuPublic.js
@@ -1,19 +1,16 @@
-let cfgfile = "hiker://files/rules/Src/Ju/config.json";
+let cfgfile = "hiker://files/rules/Src/Juyuan/config.json";
+let Juconfig = {};
 let Jucfg = fetch(cfgfile);
 if (Jucfg != "") {
-    eval("var Juconfig=" + Jucfg + ";");
-} else {
-    var Juconfig = {};
-    Juconfig["依赖"] = config.依赖 || "https://gitcode.net/src48597962/hk/-/raw/Ju/SrcJuPublic.js";
-    writeFile(cfgfile, JSON.stringify(Juconfig));
+    eval("Juconfig=" + Jucfg + ";");
 }
 
-let runTypes = ["漫画", "小说", "听书", "图集", "影视", "音频", "聚合", "其它"];
+let runTypes = ["漫画", "小说", "图集", "影视", "音频", "聚合", "其它"];
 let runMode = Juconfig["runMode"] || "漫画";
 let sourcename = Juconfig[runMode + 'sourcename'] || "";//主页源名称
 let stopTypes = storage0.getItem('stopTypes', []);
 
-let sourcefile = "hiker://files/rules/Src/Ju/jiekou.json";
+let sourcefile = "hiker://files/rules/Src/Juyuan/jiekou.json";
 let sourcedata = fetch(sourcefile);
 if (sourcedata != "") {
     try {

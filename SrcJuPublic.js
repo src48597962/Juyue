5228833095f98ed9c24f6d9a0abe951b0076b437
mybb520 <48597962@qq.com>
2025-05-19 13:46:58 +0000

    更新 SrcJuPublic.js

--- SrcJuPublic.js
+++ SrcJuPublic.js
@@ -8,7 +8,7 @@ let codepath = (config.聚阅||getPublicItem('聚阅','https://raw.gitcode.com/s
 let gzip = $.require(codepath + "plugins/gzip.js");
 
 if(!fileExist(jkfile) && fileExist("hiker://files/rules/Src/Ju/jiekou.json")){
-    writeFile(jkfile, fetch("hiker://files/rules/Src/Ju/jiekou.json").replace(/sourcename/g,'sourceName'));
+    writeFile(jkfile, fetch("hiker://files/rules/Src/Ju/jiekou.json"));
 }
 
 let Juconfig = {};
@@ -21,6 +21,7 @@ let runTypes = ["漫画", "小说", "听书", "图集", "影视", "音频", "聚
 let homeType = Juconfig["homeType"] || runTypes[0];
 let sourceName = Juconfig[homeType + '_source'] || "";//主页源名称
 let homeSourceId = sourceName ? homeType+"_"+sourceName : "";
+let sourcename = sourceName;//旧源名称
 
 //获取接口列表数据
 function getListDatas(lx, selectType, isyx) {
@@ -62,7 +63,6 @@ function getListDatas(lx, selectType, isyx) {
 
     return result;
 }
-
 //获取类型名称数组
 function getTypeNames(is) {
     let snames = [];
@@ -83,7 +83,7 @@ function getGroupNames() {
     })
     return gnames;
 }
-
+//执行切换源接口
 function changeSource(stype, sname) {
     if (homeSourceId==stype+"_"+sname) {
         return 'toast://' + stype + ' 主页源：' + sname;
@@ -485,12 +485,29 @@ function getYiData(datatype, od) {
         setResult(d);
     }
 }
-
-//简繁互转,x可不传，默认转成简体，传2则是转成繁体
-function jianfan(str, x) {
-    require(config.聚阅.replace(/[^/]*$/,'') + 'SrcSimple.js');
-    return PYStr(str, x);
+//读取接口缓存数据
+function readJkData(fileid){
+    let cachefile = cachepath + `${fileid}.json`;
+    let jkdata = {};
+    try{
+        let cachefiledata = fetch(cachefile);
+        if(cachefiledata){
+            eval("jkdata=" + cachefiledata);
+        }else{
+            let jklist = getListDatas('all', '', 1).filter(it=>{
+                return it.id == fileid;
+            });
+            if(jklist.length==1){
+                jkdata = jklist[0];
+                writeFile(cachefile,JSON.stringify(jkdata));
+            }
+        }
+    }catch(e){
+        log("接口数据加载失败>"+fileid+">"+e.message);
+    }
+    return jkdata;
 }
+
 //重定义打印日志，只允许调试模式下打印
 var xlog = log;
 log = function (msg) {
@@ -571,4 +588,112 @@ function replaceLast(str, search, replacement) {
     }
 
     return str; // 如果没找到，返回原字符串
+}
+//修正按钮元素
+function toerji(item,info) {
+    info = info || storage0.getMyVar('一级源接口信息');
+    if(item.url && !/js:|select:|=>|@|toast:|hiker:\/\/page|video:/.test(item.url) && item.col_type!="x5_webview_single" && item.url!='hiker://empty'){
+        let extra = item.extra || {};
+        extra.name = extra.name || extra.pageTitle || (item.title?item.title.replace(/‘|’|“|”|<[^>]+>/g,""):"");
+        extra.img = extra.img || item.pic_url || item.img;
+        extra.stype = info.type;
+        extra.pageTitle = extra.pageTitle || extra.name;
+        extra.surl = item.url.replace(/hiker:\/\/empty|#immersiveTheme#|#autoCache#|#noRecordHistory#|#noHistory#|#noLoading#|#/g,"");
+        extra.sname = info.name;
+        item.url = $("hiker://empty?type="+info.type+"#immersiveTheme##autoCache#").rule(() => {
+            require(config.聚阅);
+            erji();
+        })
+        item.extra = extra;
+    }
+    return item;
+}
+//简繁互转,x可不传，默认转成简体，传2则是转成繁体
+function jianfan(str,x) {
+    require(config.聚阅.match(/http(s)?:\/\/.*\//)[0] + 'SrcSimple.js');
+    return PYStr(str,x);
+}
+
+//接口管理多选方法
+function duoselect(datas){
+    let datalist = [];
+    if($.type(datas)=="array"){
+        datalist = datas;
+    }else if($.type(datas)=="object"){
+        datalist.push(datas);
+    }
+    let duoselect = storage0.getMyVar('SrcJu_duoselect')?storage0.getMyVar('SrcJu_duoselect'):[];
+    datalist.forEach(data=>{
+        let id = data.type+"_"+data.name;
+        if(!duoselect.some(item => item.name == data.name && item.type==data.type)){
+            duoselect.push(data);
+            updateItem(id, {title:'<font color=#3CB371>'+data.name + (data.parse ? " [主页源]" : "") + (data.erparse ? " [搜索源]" : "")});
+        }else{
+            for(var i = 0; i < duoselect.length; i++) {
+                if(duoselect[i].type+"_"+duoselect[i].name == id) {
+                    duoselect.splice(i, 1);
+                    break;
+                }
+            }
+            updateItem(id, {title:(data.stop?`<font color=#f20c00>`:"") + data.name + (data.parse ? " [主页源]" : "") + (data.erparse ? " [搜索源]" : "") + (data.stop?`</font>`:"")});
+        }
+    })
+    storage0.putMyVar('SrcJu_duoselect',duoselect);
+}
+//来自阿尔法大佬的主页幻灯片
+function banner(start, arr, data, cfg){
+    let id = 'juyue';
+    let rnum = Math.floor(Math.random() * data.length);
+    let item = data[rnum];
+    putMyVar('rnum', rnum);
+    let time = 5000;
+    let col_type='pic_1_card';
+    let desc='';
+    if (cfg != undefined) {
+        time = cfg.time ? cfg.time : time;
+        col_type=cfg.col_type?cfg.col_type:col_type;
+        desc=cfg.desc?cfg.desc:desc;
+    }
+    arr.push({
+        col_type: col_type,
+        img: item.img,
+        desc:desc,
+        title: item.title,
+        url: item.url,
+        extra: {
+            id: 'bar',
+        }
+    })
+    if (start == false || getMyVar('benstart', 'true') == 'false') {
+        unRegisterTask(id)
+        return
+    }
+    let obj = {
+        data: data,
+        method: config.聚阅.match(/http(s)?:\/\/.*\//)[0] + 'SrcJuMethod.js',
+        info: storage0.getMyVar('一级源接口信息')
+    };
+    registerTask(id, time, $.toString((obj) => {
+        var data = obj.data;
+        var rum = getMyVar('rnum');
+        var i = Number(getMyVar('banneri', '0'));
+        if (rum != '') {
+            i = Number(rum) + 1
+            clearMyVar('rnum')
+        } else {
+            i = i + 1;
+        }
+        if (i > data.length - 1) {
+            i = 0
+        }
+        var item = data[i];
+        try {
+            require(obj.method);
+            updateItem('bar', toerji(item,obj.info));
+        } catch (e) {
+            log(e.message)
+            unRegisterTask('juyue')
+        }
+        putMyVar('banneri', i);
+    }, obj))
 }

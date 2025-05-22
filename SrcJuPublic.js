e5b7aede51a89e25562954c5c018db01c3351db2
mybb520 <48597962@qq.com>
2025-05-22 13:49:49 +0000

    更新 SrcJuPublic.js

--- SrcJuPublic.js
+++ SrcJuPublic.js
@@ -37,7 +37,7 @@ if(!fileExist(jkfile) && fileExist("hiker://files/rules/Src/Ju/jiekou.json")){
         delete it.public;
         delete it.parse;
         delete it.erparse;
-        writeFile(newjkurl, $.stringify(newjkjson));
+        writeFile(newjkurl, JSON.stringify(newjkjson, null, 2));
     })
     writeFile(jkfile, JSON.stringify(olddatalist));
 }

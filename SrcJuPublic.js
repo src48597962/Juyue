702f48603d5b1593a52333dab5c5d93ae9d2a6e3
mybb520 <48597962@qq.com>
2025-05-24 12:27:38 +0000

    更新 SrcJuPublic.js

--- SrcJuPublic.js
+++ SrcJuPublic.js
@@ -249,9 +249,8 @@ function changeSource(sourcedata) {
     let sourceGroup = sourcedata.group || sourcedata.type;
     Juconfig[sourceGroup+'_SourceId'] = sourcedata.id;
     writeFile(cfgfile, JSON.stringify(Juconfig));
-    putMyVar(sourcedata.id, "1");//切源不二次提示
     refreshPage(false);
-    return 'toast://主页源已设置为：' + sourcedata.id;
+    return 'hiker://empty';
 }
 //封装选择主页源方法
 function selectSource(selectType) {

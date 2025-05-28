7233d55cbff7dc0791d3b5d46b704a3cc9852b03
mybb520 <48597962@qq.com>
2025-05-28 08:11:43 +0000

    更新 SrcJu.js

--- SrcJu.js
+++ SrcJu.js
@@ -66,6 +66,12 @@ function yiji(testSource) {
         })
         */
         d.push({
+            title: "管理",
+            url: "hiker://empty",
+            pic_url: "http://123.56.105.145/tubiao/more/129.png",
+            col_type: "icon_5"
+        })
+        d.push({
             title: "收藏",
             url: "hiker://collection?rule="+MY_RULE.title,
             pic_url: "http://123.56.105.145/tubiao/more/109.png",
@@ -78,24 +84,15 @@ function yiji(testSource) {
             col_type: 'icon_5'
         })
         d.push({
-            title: "预留",
-            url: "hiker://history?rule="+MY_RULE.title,
-            pic_url: "http://123.56.105.145/tubiao/more/213.png",
-            col_type: 'icon_5'
-        })
-        d.push({
             title: "聚影",
-            url: $("hiker://empty#noRefresh##noRecordHistory##noHistory##fullTheme###fypage").rule(() => {
-                require(config.依赖);
-                newsousuopage();
-            }),
+            url: "hiker://home@聚影",
             pic_url: "http://123.56.105.145/tubiao/more/101.png",
             col_type: 'icon_5'
         })
         d.push({
             title: "设置",
-            url: testSource?"toast://测试模式下不能进入设置菜单":$(["接口列表","管理中心"],1).select(()=>{
-                if(input=="本地接口管理"){
+            url: testSource?"toast://测试模式下不能进入设置菜单":$(["接口列表"],1).select(()=>{
+                if(input=="接口列表"){
                     return $("hiker://empty#noRecordHistory##noHistory##noRefresh#").rule(() => {
                         setPageTitle('本地接口管理');
                         require(config.聚阅.replace(/[^/]*$/,'') + 'SrcJuSet.js');

c9b30e65b56ff9967d092d33a20ec36134473cc9
mybb520 <48597962@qq.com>
2025-05-29 09:04:28 +0000

    更新 SrcJu.js

--- SrcJu.js
+++ SrcJu.js
@@ -267,7 +267,7 @@ function erji() {
     let erTempData = storage0.getMyVar('二级详情临时对象') || {};//二级海报等详情临时保存
     let erjiextra = storage0.getMyVar('二级附加临时对象') || MY_PARAMS;//二级换源时临时extra数据
     let jkdata = erjiextra.data;//接口数据
-    let surl = erjiextra.url;//二级请求url
+    let surl = erjiextra.url || erjiextra.surl;//二级请求url
     let sname = jkdata.name;//二级源名称
     let stype = jkdata.type;
     let sgroup = jkdata.group || jkdata.type;//二级源所在分组

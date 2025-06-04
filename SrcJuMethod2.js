ee9f5a5678c3bc7816ef724e5f5502ab255566bb
src48597962 <48597962@qq.com>
2025-06-04 02:55:19 +0000

    更新 SrcJuMethod2.js

--- SrcJuMethod2.js
+++ SrcJuMethod2.js
@@ -1,68 +1,12 @@
-//子页面读接口临时缓存
-function readData(fileid,datatype){
-    let cachefile = `hiker://files/_cache/${fileid}.json`;
-    let jkdata = {};
-    try{
-        let cachefiledata = fetch(cachefile);
-        if(cachefiledata){
-            eval("jkdata=" + cachefiledata);
-        }else{
-            let sourcefile = "hiker://files/rules/Src/Ju/jiekou.json";
-            let sourcedata = fetch(sourcefile);
-            if(sourcedata != ""){
-                try{
-                    eval("var datalist=" + sourcedata+ ";");
-                }catch(e){
-                    var datalist = [];
-                }
-            }else{
-                var datalist = [];
-            }
-            let jklist = datalist.filter(it=>{
-                return (it.type+'_'+it.name) == fileid;
-            });
-            if(jklist.length==1){
-                jkdata = jklist[0];
-                writeFile(cachefile,JSON.stringify(jkdata));
-            }
-        }
-        
-    }catch(e){
-        xlog("接口数据加载失败>"+fileid+">"+e.message);
-    }
-
-    try{
-        let parse;
-        let source;
-        if(datatype==1){
-            eval("source = " + jkdata.parse);
-            if (source && source.ext && /^http/.test(source.ext)) {
-                requireCache(source.ext, 48);
-                parse = yidata;
-            } else {
-                parse = source;
-            }
-        }else if(datatype==2){
-            eval("source = " + jkdata.erparse);
-            if (source && source.ext && /^http/.test(source.ext)) {
-                requireCache(source.ext, 48);
-                parse = erdata;
-            } else {
-                parse = source;
-            }
-        }else if(datatype==3){
-            eval("source = " + jkdata.public);
-            if (source && source.ext && /^http/.test(source.ext)) {
-                requireCache(source.ext, 48);
-                parse = ggdata;
-            } else {
-                parse = source;
-            }
-        }
+//子页面读接口规则数据
+function readData(fileid){
+    let file = `hiker://files/rules/Src/Juyue/jiekou/${fileid}.txt`;
+    let filedata = fetch(file);
+    if(filedata){
+        eval(filedata);
         return parse;
-    }catch(e){
-        xlog("读取接口本地缓存文件失败>"+e.message);
-        return "";
+    }else{
+        return {};
     }
 }
 let 一级 = function(fileid) {

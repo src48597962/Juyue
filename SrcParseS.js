2de90ee0ff63ab0d7ada00c37e964acde13b3d28
src48597962 <48597962@qq.com>
2025-06-05 06:54:23 +0000

    更新 SrcParseS.js

--- SrcParseS.js
+++ SrcParseS.js
@@ -25,7 +25,7 @@ function exeWebRule(webObj, music, js) {
                     }
                 }else if (contain.test(urls[i])&&!exclude.test(urls[i])) {
                     fba.clearVar('getParse');
-                    //fba.log("exeWebRule解析到>"+urls[i]);
+                    fba.log("exeWebRule解析到>"+urls[i]);
                     return fy_bridge_app.getHeaderUrl(urls[i]) + '#isVideo=true#';
                 }
             }

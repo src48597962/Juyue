866ead59632be23673cbbabd60816eb3fa954e4a
src48597962 <48597962@qq.com>
2025-06-06 04:12:04 +0000

    更新 SrcJu.js

--- SrcJu.js
+++ SrcJu.js
@@ -133,7 +133,7 @@ function yiji(testSource) {
             col_type: 'icon_5'
         })
         d.push({
-            title: "拾光",
+            title: "收藏",
             url: $("hiker://empty###noRecordHistory##noHistory#").rule(() => {
                 require(config.聚阅.match(/http(s)?:\/\/.*\//)[0] + 'SrcBookCase.js');
                 bookCase();

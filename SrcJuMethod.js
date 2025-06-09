16a89d8aa583721c8495ea5d4afe8c6cc7acd008
src48597962 <48597962@qq.com>
2025-06-09 13:19:38 +0000

    更新 SrcJuMethod.js

--- SrcJuMethod.js
+++ SrcJuMethod.js
@@ -325,7 +325,9 @@ function toerji(item, jkdata) {
         if(!jkdata.url){
             jkdata = storage0.getMyVar('一级源接口信息');
         }
-        if(item.url && !/js:|select:|=>|@|toast:|hiker:\/\/page|video:/.test(item.url) && item.col_type!="x5_webview_single" && item.url!='hiker://empty'){
+        if(item.url && item.url.includes('#notoerji#')){
+            item.url = item.url.replace('#notoerji#', '');
+        }else if(item.url && !/js:|select:|=>|@|toast:|hiker:\/\/page|video:/.test(item.url) && item.col_type!="x5_webview_single" && item.url!='hiker://empty'){
             let extra = item.extra || {};
             extra.name = extra.name || extra.pageTitle || (item.title?item.title.replace(/‘|’|“|”|<[^>]+>/g,""):"");
             extra.img = extra.img || item.pic_url || item.img;

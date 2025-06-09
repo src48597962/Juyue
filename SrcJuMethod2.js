1c176ba57743a428e20ac4a7d9db931cca8c20d9
src48597962 <48597962@qq.com>
2025-06-09 23:58:20 +0000

    更新 SrcJuMethod2.js

--- SrcJuMethod2.js
+++ SrcJuMethod2.js
@@ -116,7 +116,7 @@ function compress(bmpOriginal, ratio) {
 }
 
 let exports = {
-    "parse": parse,
+    "parse": () => parse(),
     "imageDecrypt": (key, iv, kiType, mode) => 图片解密(input, key, iv, kiType, mode),
     "imgDec": (key, iv, kiType, mode, base64Dec) => 图片解密2(key, iv, kiType, mode, base64Dec),
     "compress": (ratio) => compress(input, ratio),

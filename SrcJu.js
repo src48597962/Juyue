c8b3e6fb04a499b3c2c11e34ebd0af148798c8fc
src48597962 <48597962@qq.com>
2025-06-12 14:46:45 +0000

    更新 SrcJu.js

--- SrcJu.js
+++ SrcJu.js
@@ -1081,7 +1081,7 @@ function erji() {
                         delete parse[it];
                     })
                 }catch(e){}
-                erjiextra.data.extstr = fetch(jkdata.url);//'let parse = ' + parse.toString();
+                erjiextra.data.extstr = 'let parse = ' + parse.toString();
                 setPageParams(erjiextra);
             }
         }

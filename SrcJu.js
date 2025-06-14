a57a77906991a9b9b684cd89101e37be60c851b9
src48597962 <48597962@qq.com>
2025-06-14 08:54:10 +0000

    更新 SrcJu.js

--- SrcJu.js
+++ SrcJu.js
@@ -284,7 +284,7 @@ function yiji(testSource) {
                                 }
                             });
                         })
-                        addItemAfter(homesousuoid);
+                        addItemAfter('homesousuoid', d);
                     }
                 }, searchurl)
             }

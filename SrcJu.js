1f1603d2fb307c829b5aeaaace5c3fa3c4613f80
src48597962 <48597962@qq.com>
2025-06-14 07:04:27 +0000

    更新 SrcJu.js

--- SrcJu.js
+++ SrcJu.js
@@ -356,7 +356,7 @@ function erji() {
     let pageid = smark.pageid || 0;//分页索引id
 
     if(jkdata.extstr){
-        writeFile(jkdata.url.replace('rules/Src','_cache'), jkdata.extstr.toString());
+        saveFile(jkdata.url.replace('rules/Src','_cache'), jkdata.extstr);
         //if(!fileExist(jkdata.url) && !fileExist(jkdata.url.replace('rules/Src','_cache'))){
         //    writeFile(jkdata.url.replace('rules/Src','_cache'), jkdata.extstr);
         //}

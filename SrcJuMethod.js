1cc05dbd4118fa9dac4ae23fd1407a778367d424
src48597962 <48597962@qq.com>
2025-06-14 14:51:19 +0000

    更新 SrcJuMethod.js

--- SrcJuMethod.js
+++ SrcJuMethod.js
@@ -329,7 +329,7 @@ function getObjCode(jkdata, key) {
                 let delarr = [];
                 switch (key) {
                     case 'yi':
-                        delarr = ['二级','最新','解析'];
+                        delarr = ['二级','最新'];
                         break;
                     case 'er':
                         delarr = ['主页','分类','排序','更新','搜索','解析'];

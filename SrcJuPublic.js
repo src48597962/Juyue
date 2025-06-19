7b1c0e588d3750024b952b79501c7b52151a9e32
src48597962 <48597962@qq.com>
2025-06-19 10:12:40 +0000

    更新 SrcJuPublic.js

--- SrcJuPublic.js
+++ SrcJuPublic.js
@@ -271,7 +271,7 @@ function colorTitle(title, Color) {
 // 获取接口对应的显示标题
 function getDataTitle(data) {
     if((MY_NAME=="海阔视界"&&getAppVersion()>=5566)||(MY_NAME=="嗅觉浏览器"&&getAppVersion()>=2305)){
-        return data.name + '  ‘‘’’<small><font color=grey>'+(data.author?'  ('+data.author+')':'') + '</font></small>';
+        return data.name + '  ‘‘’’<small><font color=grey>'+(data.author?'  ['+data.author+']':'') + '</font></small>';
     }else{
         return data.name + '  <small><font color=grey>'+(data.author?' ('+data.author+')':'') + (data.ilk=="1" ? " [主页源]" : data.ilk=="2" ? " [搜索源]" : " [完整源]") + '</font></small>';
     }

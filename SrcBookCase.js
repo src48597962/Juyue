54fffb39eb743c21dd2f8ca62641e2592e89f610
src48597962 <48597962@qq.com>
2025-06-30 09:31:16 +0000

    更新 SrcBookCase.js

--- SrcBookCase.js
+++ SrcBookCase.js
@@ -48,7 +48,7 @@ function bookCase() {
             refreshPage(false);
             return 'hiker://empty';
         }),
-        img: getIcon(sjIcons[1].img, false, sjIcons[2].color),
+        img: getIcon(sjIcons[1].img, false, sjIcons[1].color),
         col_type: "icon_small_3"
     });
     for (let i = 0; i < 8; i++) {

24e0edb4b3d34df72cf8140317ed8bb9dd26985e
src48597962 <48597962@qq.com>
2025-07-02 08:11:17 +0000

    更新 SrcBookCase.js

--- SrcBookCase.js
+++ SrcBookCase.js
@@ -128,7 +128,9 @@ function bookCase() {
                             title: col_type=='movie_1_vertical_pic'?name.substring(0,15) + "\n\n‘‘’’<small>💠  <font color=#bfbfbf>"+(stype?stype+" | "+(sname||""):"自开二级页面")+"</font></small>":name,
                             pic_url: it.picUrl,
                             desc: col_type=='movie_1_vertical_pic'?"🕓 "+mask.substring(0,15)+"\n\n🔘 "+last:last,
-                            url: url + (url.startsWith('hiker://page/')?'':'@rule=' + params.find_rule),
+                            url: $('#noLoading#').lazyRule((go)=>{
+                                return go;
+                            }, url + (url.startsWith('hiker://page/')?'':'@rule=' + params.find_rule)),
                             col_type: col_type,
                             extra: extra
                         })

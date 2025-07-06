2005a0532b20e3920815c920b14cd7bb043f6652
src48597962 <48597962@qq.com>
2025-07-06 08:22:07 +0000

    更新 SrcJu.js

--- SrcJu.js
+++ SrcJu.js
@@ -532,14 +532,11 @@ function erji() {
                         eval("let cacheJson=" + cacheData + ";");
                         let nowtime = Date.now();
                         let oldtime = cacheJson.updatetime||0;
-                        xlog(nowtime.toString());
-                        xlog((oldtime + 1 * 60 * 60 * 1000).toString());
-                        if(cacheJson.sid==sid && cacheJson.url==MY_URL && nowtime > (oldtime + 1 * 60 * 60 * 1000)){
-                            xlog('读到');
+                        if(cacheJson.sid==sid && cacheJson.url==MY_URL && nowtime < (oldtime + 1 * 60 * 60 * 1000)){
                             erdataCache = cacheJson;//本地缓存接口+链接对得上则取本地，用于切换排序和样式时加快，缓存1小时
                         }
                     }catch(e){
-                        xlog('读取二级缓存失败>' + e.message);
+                        //xlog('读取二级缓存失败>' + e.message);
                     }
                 }
             }

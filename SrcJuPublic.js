8f3f7a23b2877c99f31af806efd092a812db7ac6
src48597962 <48597962@qq.com>
2025-07-14 00:15:56 +0000

    更新 SrcJuPublic.js

--- SrcJuPublic.js
+++ SrcJuPublic.js
@@ -275,9 +275,9 @@ function colorTitle(title, Color) {
 // 获取接口对应的显示标题
 function getDataTitle(data, ide) {
     if((MY_NAME=="海阔视界"&&getAppVersion()>=5566)||(MY_NAME=="嗅觉浏览器"&&getAppVersion()>=2305)){
-        return (ide||(getMyVar('批量选择模式')?'○':''))+(data.stop?'Ⓔ':'')+data.name + '  ‘‘’’<small><font color=grey>'+(data.author?'  ['+data.author+']':'') + '</font></small>';
+        return (ide||(getMyVar('批量选择模式')?'○':''))+(data.stop?'Ⓓ':'')+data.name + '  ‘‘’’<small><font color=grey>'+(data.author?'  ['+data.author+']':'') + '</font></small>';
     }else{
-        return (ide||(getMyVar('批量选择模式')?'○':''))+(data.stop?'Ⓔ':'')+data.name + '  <small><font color=grey>'+(data.author?' ('+data.author+')':'') + (data.ilk=="1" ? " [主页源]" : data.ilk=="2" ? " [搜索源]" : " [完整源]") + '</font></small>';
+        return (ide||(getMyVar('批量选择模式')?'○':''))+(data.stop?'Ⓓ':'')+data.name + '  <small><font color=grey>'+(data.author?' ('+data.author+')':'') + (data.ilk=="1" ? " [主页源]" : data.ilk=="2" ? " [搜索源]" : " [完整源]") + '</font></small>';
     }
 }
 // 接口多选处理方法

05a6945d282f8d7714b992566b8bb7b812277ef6
src48597962 <48597962@qq.com>
2025-07-17 08:56:39 +0000

    更新 SrcJuMethod.js

--- SrcJuMethod.js
+++ SrcJuMethod.js
@@ -131,54 +131,6 @@ function createClass(d, obj) {
         }
         MY_URL = obj.url.replace(/fyAll/g, fyAll).replace(/fyclass/g, fyclass).replace(/fyarea/g, fyarea).replace(/fyyear/g, fyyear).replace(/fysort/g, fysort);
         
-        function extractFypageParams(str) {
-            const regex = /fypage@((?:[^@$$;]+@)+)(?=[[$$;]|$)/;
-            const match = str.match(regex);
-            if (!match) return null;
-
-            const paramStr = match[1]; // 获取参数部分（含多个@）
-            const params = paramStr.split('@').filter(Boolean); // 拆分成数组并过滤空值
-            return params;
-        }
-
-        function calculateOffset(params, currentPage) {
-            let result = currentPage;
-            for (const param of params || []) {
-                const op = param.match(/^([+\-*/])/)?.[1] || '+';
-                const valueStr = param.replace(/^[+\-*/]/, '');
-                const value = parseInt(valueStr) || 0;
-
-                switch (op) {
-                    case '+': result += value; break;
-                    case '-': result -= value; break;
-                    case '*': result *= value; break;
-                    case '/': result /= value; break;
-                }
-            }
-            return result;
-        }
-
-        function generatePageUrl(url, page) {
-            // 处理首页特殊规则
-            const firstPageMatch = url.match(/\[firstPage=(.*?)\]/);
-            if (page === 1 && firstPageMatch) {
-                return firstPageMatch[1];
-            }
-
-            let resultUrl = url.replace(/\[firstPage=(.*?)\]/, '');
-
-            // 提取并处理 fypage@...@ 的参数
-            const params = extractFypageParams(resultUrl);
-            if (params) {
-                const newPage = calculateOffset(params, page);
-                const fypageRegex = /fypage@(?:[^@$$;]+@)+(?=[[$$;]|$)/;
-                resultUrl = resultUrl.replace(fypageRegex, newPage.toString());
-            } else {
-                resultUrl = resultUrl.replace(/fypage/g, page.toString());
-            }
-
-            return resultUrl;
-        }
         let fypage = MY_PAGE;
         MY_URL = MY_URL.replace(/fypage/g, fypage);
         //MY_URL = generatePageUrl(MY_URL, MY_PAGE);

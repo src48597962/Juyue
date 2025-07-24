aeb0f0c6cd7f4cca357dac0a38f74cf1019638d0
src48597962 <48597962@qq.com>
2025-07-24 03:47:23 +0000

    更新 SrcJuSet.js

--- SrcJuSet.js
+++ SrcJuSet.js
@@ -312,7 +312,7 @@ function SRCSet() {
 
         d.push({
             title: "批量检测",
-            url: !config.聚阅.includes('1300')?"toast://暂未上线":$('#noLoading#').lazyRule(() => {
+            url: !config.聚阅.includes('000')?"toast://暂未上线":$('#noLoading#').lazyRule(() => {
                 let nowtime = Date.now();
                 let checkSourcetime = getItem('checkSourcetime','0');
                 let oldtime = parseInt(checkSourcetime.split('|')[0]);

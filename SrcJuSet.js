5847a466d34969cf930a8c5a31f83446693bd961
src48597962 <48597962@qq.com>
2025-07-26 14:40:52 +0000

    更新 SrcJuSet.js

--- SrcJuSet.js
+++ SrcJuSet.js
@@ -1190,6 +1190,9 @@ function jiekousave(urls, mode) {
             if(it.oldid || mode==1){//覆盖已存在接口
                 for(let i=0;i<datalist.length;i++){
                     if(datalist[i].id==it.id || datalist[i].id==it.oldid){
+                        if(mode==1 && !datalist[i].group){
+                            it.group = datalist[i].group;
+                        }
                         datalist.splice(i,1);
                         break;
                     }

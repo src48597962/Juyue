0ded3c072be73c3db33b4e3127a5ece97a690c4c
src48597962 <48597962@qq.com>
2025-07-28 13:32:53 +0000

    更新 SrcJu.js

--- SrcJu.js
+++ SrcJu.js
@@ -1561,15 +1561,16 @@ function erjisousuo(name,group,datas,sstype) {
         }
 
         let task = function (obj) {
-            //return (function() {
+            return (function() {
                 try {
+                    MY_RULE.id = obj.data.name;
                     let lists = obj.search(obj.name, obj.type, obj.data);
                     return {result:lists, success:1, type: obj.type, name: obj.data.name};
                 } catch (e) {
                     xlog(obj.data.name + '>搜索失败>' + e.message);
                     return {result:[], success:0, type: obj.type, name: obj.data.name};
                 }
-            //})();
+            })();
         }
         let list = ssdatalist.map((item) => {
             return {

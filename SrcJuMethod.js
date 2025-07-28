ca6edea6101e667195021de471ebae0eccb2a2fd
src48597962 <48597962@qq.com>
2025-07-28 07:21:29 +0000

    更新 SrcJuMethod.js

--- SrcJuMethod.js
+++ SrcJuMethod.js
@@ -24,7 +24,7 @@ let juItem = {
     'put': function(key, s, id){
         id = id || (storage0.getMyVar('二级源接口信息') || storage0.getMyVar('一级源接口信息')).id;
         xlog(id);
-        xlog(parse.id);
+        //xlog(parse.id);
         xlog(jkdata.id);
         xlog(this.id);
         let items = juItem.items();

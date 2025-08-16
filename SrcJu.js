378166dbf5f81262a126f7d5b265b0bab31e00ac
src48597962 <48597962@qq.com>
2025-08-16 08:39:57 +0000

    更新 SrcJu.js

--- SrcJu.js
+++ SrcJu.js
@@ -2137,7 +2137,7 @@ function bookCase() {
             }
 
             datalist.push({//#bfbfbf
-                title: col_type=='movie_1_vertical_pic'?name.substring(0,15) + "\n\n‘‘’’<small><font color=grey>"+(stype?stype+" | "+(sname||""):"自开二级页面")+"</font></small>":name,
+                title: col_type=='movie_1_vertical_pic'?name.substring(0,15) + "\n\n‘‘’’<small><font color=grey>"+(stype?stype+" | "+(sname||""):"")+"</font></small>":name,
                 pic_url: it.picUrl,
                 desc: col_type=='movie_1_vertical_pic'?lastChapter+"\n\n足迹："+(it.lastClick||'').substring(0,15):lastChapter.replace('更新至：',''),
                 url: url,

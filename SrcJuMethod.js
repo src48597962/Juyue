b68970c846f14d0e6d9e89ba603c66a487498f2d
src48597962 <48597962@qq.com>
2025-08-27 07:56:16 +0000

    更新 SrcJuMethod.js

--- SrcJuMethod.js
+++ SrcJuMethod.js
@@ -473,7 +473,7 @@ function getYiData(datatype, jkdata, dd) {
 function ocr(codeurl,headers) {
     headers= headers || {};
     let img = convertBase64Image(codeurl,headers).replace('data:image/jpeg;base64,','');
-    let code = request('https://api-cf.nn.ci/ocr/b64/text', { body: img, method: 'POST', headers: {"Content-Type":"text/html"}});
+    let code = request('https://openlistteam-ocr-api-server.hf.space/ocr/file/json', { body: img, method: 'POST', headers: {"Content-Type":"text/html"}});
     code = code.replace(/o/g, '0').replace(/u/g, '0').replace(/I/g, '1').replace(/l/g, '1').replace(/g/g, '9');
     if(code.includes("+")&&code.includes("=")){
         code = eval(code.split("=")[0]);

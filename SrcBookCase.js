3df9b911607fb5ab0c9ca6b9adc7087844876ef5
src48597962 <48597962@qq.com>
2025-09-01 08:40:00 +0000

    更新 SrcBookCase.js

--- SrcBookCase.js
+++ SrcBookCase.js
@@ -1,3 +1,6 @@
+
+//本代码仅用于个人学习，请勿用于其他作用，下载后请24小时内删除，代码虽然是公开学习的，但请尊重作者，应留下说明
+require((config.聚阅||getPublicItem('聚阅','')).replace(/[^/]*$/,'') + 'SrcJuPublic.js');
 //书架
 function bookCase() {
     addListener("onClose", $.toString(() => {

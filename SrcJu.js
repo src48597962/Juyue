1becf8434d1ac76031a157c7685e74783d98f4cc
src48597962 <48597962@qq.com>
2025-09-01 04:02:16 +0000

    更新 SrcJu.js

--- SrcJu.js
+++ SrcJu.js
@@ -1433,7 +1433,7 @@ function erji() {
             xlog(sname + '>加载二级页面失败>' + e.message + ' 错误行#' + e.lineNumber);
         }
         d.push({
-            title: "‘‘’’<small><small><font color=#bfbfbf>当前数据源：" + sname + (erLoadData.author?", 作者：" + erLoadData.author:"") + (parse['tmplname']?"，模板："+parse['tmplname']:"") + "</font></small></small>",
+            title: "‘‘’’<small><small><font color=#bfbfbf>当前数据源：" + sname + (erLoadData.author?", 作者：" + erLoadData.author:"") + (parse['模板名']?"，模板："+parse['模板名']:"") + "</font></small></small>",
             url: stype=="小说"?'hiker://empty':'toast://温馨提示：且用且珍惜！',
             col_type: getMyVar('线路显示翻页内容')?'blank_block':'text_center_1',
             extra: {

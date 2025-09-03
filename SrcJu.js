675772ba5357726f7d7c1f65703bda629bbf29a6
src48597962 <48597962@qq.com>
2025-09-03 15:41:06 +0000

    更新 SrcJu.js

--- SrcJu.js
+++ SrcJu.js
@@ -582,7 +582,7 @@ function erji() {
 
     if(MY_PAGE==1){
         try {
-            let detailObj = ((juItem.get('二级聚阅封面')||(juItem.get('二级聚阅封面')&&juItem2.get('二级聚阅封面')))&&erLoadData.detail1?{}:erLoadData.detailObj) || {}; //二级是否有传封面对象，有传就优先使用
+            let detailObj = ((juItem.get('二级聚阅封面')||juItem2.get('二级聚阅封面'))&&erLoadData.detail1?{}:erLoadData.detailObj) || {}; //二级是否有传封面对象，有传就优先使用
             pic = erLoadData.img || oldMY_PARAMS.img;// || "https://p1.ssl.qhimgs1.com/sdr/400__/t018d6e64991221597b.jpg";
 
             erjiextra.img = pic;
@@ -1039,7 +1039,7 @@ function erji() {
                 morecols.push("选集样式:"+getItem('SrcJuList_col_type', '自动'))
                 morecols.push("二级简洁模式:"+(juItem2.get('二级简洁模式')?"是":"否"))
                 if(erLoadData.detail1 && erLoadData.detailObj){
-                    morecols.push("自定义封面样式:"+((juItem.get('二级聚阅封面')||(juItem.get('二级聚阅封面')&&juItem2.get('二级聚阅封面')))?"否":"是"))
+                    morecols.push("自定义封面样式:"+((juItem.get('二级聚阅封面')||juItem2.get('二级聚阅封面'))?"否":"是"))
                 }
                 
                 d.push({
@@ -1128,13 +1128,13 @@ function erji() {
                             return "toast://"+sm;
                         }else if(input.includes('自定义封面样式')){
                             let list = [];
-                            list.push('所有源:'+(juItem2.get('二级聚阅封面')?"关":"开"));
-                            list.push('当前源:'+(juItem.get('二级聚阅封面')?"关":"开"));
+                            list.push('所有源:'+(juItem2.get('二级聚阅封面')?"关":juItem2.get('二级聚阅封面')=='0'?"开":""));
+                            list.push('当前源:'+(juItem.get('二级聚阅封面')?"关":juItem.get('二级聚阅封面')=='0'?"开":""));
                             return $(list, 2, '自定义封面开关，关代表不显示').select(()=>{
                                 let sm;
                                 if(input.includes('当前源')){
                                     if(juItem.get('二级聚阅封面')){
-                                        juItem.clear('二级聚阅封面');
+                                        juItem.set('二级聚阅封面','0');
                                         sm = "当前源优先自定义封面样式";
                                     }else{
                                         juItem.set('二级聚阅封面','1');
@@ -1142,7 +1142,7 @@ function erji() {
                                     }
                                 }else{
                                     if(juItem2.get('二级聚阅封面')){
-                                        juItem2.clear('二级聚阅封面');
+                                        juItem2.set('二级聚阅封面','0');
                                         sm = "所有源优先自定义封面样式";
                                     }else{
                                         juItem2.set('二级聚阅封面','1');

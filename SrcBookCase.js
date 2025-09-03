b67ed3007c33adaf1bda16ff7496637eaec0cd38
src48597962 <48597962@qq.com>
2025-09-03 08:59:50 +0000

    更新 SrcBookCase.js

--- SrcBookCase.js
+++ SrcBookCase.js
@@ -508,6 +508,7 @@ function convertItem(item, listcol, sjType){
         }else if(listcol=='movie_3_marquee'){
             itemdesc = item.lastChapter.replace('更新至：','');
         }else if(listcol=='icon_1_left_pic'){
+            log(item.lastChapter);
             let lastChapter = item.lastChapter.startsWith(sname)?item.lastChapter:"";//(sname+(item.lastChapter?(" | "+item.lastChapter):""))
             itemtitle = name.substring(0,13) + "\n‘‘’’<small><font color=grey>"+lastChapter+"</font></small>";
             itemdesc = "足迹："+item.lastClick;

375c134810f50839c67925747d4b8089d3fbd021
src48597962 <48597962@qq.com>
2025-09-04 15:40:04 +0000

    添加 template/采集cms.js

---
+++ template/采集cms.js
@@ -0,0 +1,20 @@
+let parse = {
+    作者: "",
+    配置: {
+        title: "", //资源网名称
+        host: "", //采集接口地址
+        //parse_url: "https://jx.yparse.com/index.php?url=",
+        //搜索: false
+    },
+    模板: {
+        id: '1756685891015',
+        name: '采集cms'
+    },
+    /*//此为自定义解析逻辑，需要则去除注释
+    解析: function(url, appconfig){
+        appconfig = parse.配置;
+        let {url,parse_api_url,token,from} = JSON.parse(url);
+        ...
+    }
+    */
+}

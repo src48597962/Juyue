baf054885f64c4bbc8d1f26880d73c40eaa6f2eb
src48597962 <48597962@qq.com>
2025-09-04 03:19:27 +0000

    添加 template/tmplCode.js

---
+++ template/tmplCode.js
@@ -0,0 +1,16 @@
+//模板源接口标准格式，需要配合对应的模板源
+let parse = {
+    作者: "",
+    模板: {
+        id: '',
+        name: ''
+    },
+    /*//此为自定义解析逻辑，需要则去除注释
+    解析: function(url){
+        appconfig = parse.配置;
+        let {url,parse_api_url,token,from} = JSON.parse(url);
+        ...
+    }
+    //其他需要属性也是一样，需要可以自己写，会加载覆盖到模板中
+    */
+};

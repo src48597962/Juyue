d61dbb598bcab28824bb6913b2bfdb0d84a1ca09
src48597962 <48597962@qq.com>
2025-09-05 03:14:39 +0000

    更新 SrcJuPublic.js

--- SrcJuPublic.js
+++ SrcJuPublic.js
@@ -378,7 +378,7 @@ function changeSource(sourcedata) {
     if (homeSourceId==sourcedata.id) {
         return 'toast://主页源：' + sourcedata.name;
     }
-    setJkSort(sourcedata, {use: 1});
+    setJkSort(sourcedata.id, {use: 1});
     if (typeof (unRegisterTask) != "undefined") {
         unRegisterTask("juyue");
         unRegisterTask("lunbo");

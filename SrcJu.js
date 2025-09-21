744b67b752fd919b1229b2b658b09f819e8322d4
src48597962 <48597962@qq.com>
2025-09-21 15:07:43 +0000

    更新 SrcJu.js

--- SrcJu.js
+++ SrcJu.js
@@ -1542,12 +1542,7 @@ function erji() {
                 writeFile(erCacheFile, $.stringify(erLoadData));//第一次打开页面保存缓存
                 let caseObj = erLoadData.caseData;
                 if(getItem("自动更新二级源接口")=="0" && !getMyVar('二级切换站源')){
-                    caseObj = {
-                        id: erLoadData.caseData.id,
-                        params: {
-                            url: erLoadData.caseData.params.url
-                        }
-                    }
+                    delete caseObj.params.params;
                 }
                 addBookCase(caseObj, true);//更新收藏书架数据
             }else if(saveCache){

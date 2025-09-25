472f4577bd9c3182a47e675d4a81837888a5c8ec
src48597962 <48597962@qq.com>
2025-09-25 01:59:06 +0000

    更新 SrcJuMethod.js

--- SrcJuMethod.js
+++ SrcJuMethod.js
@@ -52,6 +52,7 @@ function juItemF(id, s){
         'get': function (key, str, id2) {
             if(!key) return;
             id = id2 || id || (storage0.getMyVar('二级源接口信息') || storage0.getMyVar('一级源接口信息')).id;
+            log(key+">"+id);
             if(!id){
                 xlog(key+':id获取失败');
                 return;

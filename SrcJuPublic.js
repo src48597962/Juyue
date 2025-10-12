93724ed3191084e3ec073e9528e6dceeb88d0ef9
src48597962 <48597962@qq.com>
2025-10-12 13:46:30 +0000

    更新 SrcJuPublic.js

--- SrcJuPublic.js
+++ SrcJuPublic.js
@@ -1077,7 +1077,6 @@ function testSourceS(){
         return (function() {
             let msg;
             try{
-                let MY_PAGE =1;
                 let result = getTestData('主页', jkdata);
                 if(result.error){
                     msg = result.message;

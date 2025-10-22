997897cdb5b5a50d37fcc390f68501db3781dccd
src48597962 <48597962@qq.com>
2025-10-22 04:34:28 +0000

    更新 SrcJuMethod.js

--- SrcJuMethod.js
+++ SrcJuMethod.js
@@ -322,7 +322,7 @@ function getYiData(datatype, jkdata, dd) {
             return;
         }
         
-        xlog('开始获取一级数据');
+        xlog('开始获取'+datatype+' 第'+page+'页数据');
         let t1 = new Date().getTime();
         let ide = parse["频道"].页面标识 || "";
         
@@ -500,7 +500,7 @@ function getYiData(datatype, jkdata, dd) {
         }
         deleteItemByCls("loading_gif");
         let t2 = new Date().getTime();
-        xlog('获取'+datatype+'第'+page+'页数据完成，耗时：' + (t2-t1) + 'ms');
+        xlog('获取'+datatype+'数据完成，耗时：' + (t2-t1) + 'ms');
     } catch (e) {
         toast(datatype + "执行获取代码报错，请查看日志");
         xlog("报错信息>" + e.message + " 错误行#" + e.lineNumber);

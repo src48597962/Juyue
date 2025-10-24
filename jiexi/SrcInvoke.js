d21b27357223ca32a72a22bfe8fbdae96d0067bf
src48597962 <48597962@qq.com>
2025-10-24 09:16:59 +0000

    更新 jiexi/SrcInvoke.js

--- jiexi/SrcInvoke.js
+++ jiexi/SrcInvoke.js
@@ -1,14 +1,19 @@
 // 解析调用入口
 const codePath = module.modulePath.slice(0, module.modulePath.lastIndexOf("/") + 1);
+
+initConfig({
+    jxCodePath: codePath
+})
+
 // 解析管理页面
 function jxItem() {
     require(codePath + 'SrcJiexi.js');
     jiexi();
 }
-
-initConfig({
-    jxCodePath: codePath
-})
+// 调用解析
+function parse() {
+    
+}
 
 $.exports = {
     jxItem: jxItem,

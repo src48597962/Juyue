c907532863e31c55cefd2302fd5a3920802f4760
src48597962 <48597962@qq.com>
2025-10-30 08:20:55 +0000

    更新 jiexi/SrcJiexi.js

--- jiexi/SrcJiexi.js
+++ jiexi/SrcJiexi.js
@@ -17,10 +17,11 @@ function homePage() {
         title: getMyVar('主页显示内容', '1')=="1"?`““””<b><span style="color: #3399cc">解析列表</span></b>`:'解析列表',
         url: $('#noLoading#').lazyRule(() => {
             putMyVar('主页显示内容', '1');
+            refreshPage();
             return "hiker://emtpy";
         }),
         img: 'http://123.56.105.145/tubiao/system/42.png',
-        col_type: "icon_3_fill"//icon_small_3
+        col_type: "icon_small_3"
     });
     dd.push({
         title: '',
@@ -32,10 +33,11 @@ function homePage() {
         title: getMyVar('主页显示内容', '1')=="2"?`““””<b><span style="color: #3399cc">解析设置</span></b>`:'解析设置',
         url: $('#noLoading#').lazyRule(() => {
             putMyVar('主页显示内容', '2');
+            refreshPage();
             return "hiker://emtpy";
         }),
         img: 'http://123.56.105.145/tubiao/system/43.png',
-        col_type: "icon_3_fill"
+        col_type: "icon_small_3"
     });
     setPreResult(dd);
     

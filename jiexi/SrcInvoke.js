f736d4c45a8488e7f1ae8a09c1c0ae754de36fb1
src48597962 <48597962@qq.com>
2025-11-04 12:29:58 +0000

    更新 jiexi/SrcInvoke.js

--- jiexi/SrcInvoke.js
+++ jiexi/SrcInvoke.js
@@ -23,7 +23,7 @@ function home() {
             return "hiker://emtpy";
         }),
         img: 'http://123.56.105.145/tubiao/system/42.png',
-        col_type: "icon_3"
+        col_type: "icon_small_3"
     });
     d.push({
         title: getMyVar('主页显示内容', '1')=="2"?`‘‘’’<b><span style="color: `+Color+`">调用管理</span></b>`:'调用管理',
@@ -33,7 +33,7 @@ function home() {
             return "hiker://emtpy";
         }),
         img: 'http://123.56.105.145/tubiao/system/42.png',
-        col_type: "icon_3"
+        col_type: "icon_small_3"
     });
     d.push({
         title: getMyVar('主页显示内容', '1')=="3"?`‘‘’’<b><span style="color: `+Color+`">解析设置</span></b>`:'解析设置',
@@ -43,7 +43,7 @@ function home() {
             return "hiker://emtpy";
         }),
         img: 'http://123.56.105.145/tubiao/system/43.png',
-        col_type: "icon_3"
+        col_type: "icon_small_3"
     });
     
     if(getMyVar('主页显示内容', '1')=='1'){

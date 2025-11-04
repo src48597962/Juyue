a393aa05350711e640cc2f86727bc4459e2b571c
src48597962 <48597962@qq.com>
2025-11-04 08:20:40 +0000

    更新 jiexi/SrcJiexi.js

--- jiexi/SrcJiexi.js
+++ jiexi/SrcJiexi.js
@@ -19,6 +19,16 @@ function homePage() {
         col_type: "icon_3"
     });
     d.push({
+        title: getMyVar('主页显示内容', '1')=="2"?`‘‘’’<b><span style="color: `+Color+`">列表</span></b>`:'解析列表',
+        url: $('#noLoading#').lazyRule(() => {
+            putMyVar('主页显示内容', '2');
+            refreshPage();
+            return "hiker://emtpy";
+        }),
+        img: 'http://123.56.105.145/tubiao/system/42.png',
+        col_type: "icon_2"
+    });
+    d.push({
         title: getMyVar('主页显示内容', '1')=="3"?`‘‘’’<b><span style="color: `+Color+`">解析设置</span></b>`:'解析设置',
         url: $('#noLoading#').lazyRule(() => {
             putMyVar('主页显示内容', '3');

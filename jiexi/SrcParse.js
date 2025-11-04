0789ef59147a22cb6925fc486b3369a1becb9da6
src48597962 <48597962@qq.com>
2025-11-04 14:31:10 +0000

    更新 jiexi/SrcParse.js

--- jiexi/SrcParse.js
+++ jiexi/SrcParse.js
@@ -101,7 +101,8 @@ function SrcParse(vipUrl, dataObj) {
     dataObj = dataObj || {};
     let isVip = 0;
     log("请求地址："+vipUrl); 
-
+    require(config.jxCodePath + 'SrcPublic.js');
+    if(ism)
     if(vipUrl.startsWith('ftp://') && vipUrl.includes('114s.com')){
         if(!fileExist("hiker://files/cache/bidi.dex") || !fileExist("hiker://files/cache/libp2p.so")){
             return "toast://缺少荐片插件，播放失败";

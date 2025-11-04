bd227fbc0892dc092dcfe539d41bf9d5f49b8365
src48597962 <48597962@qq.com>
2025-11-04 12:02:19 +0000

    更新 jiexi/SrcInvoke.js

--- jiexi/SrcInvoke.js
+++ jiexi/SrcInvoke.js
@@ -1,27 +1,70 @@
 // 解析调用入口
-const codePath = module.modulePath.slice(0, module.modulePath.lastIndexOf("/") + 1);
+const jxCodePath = module.modulePath.slice(0, module.modulePath.lastIndexOf("/") + 1);
 
 initConfig({
-    jxCodePath: codePath
+    jxCodePath: jxCodePath
 })
 // 主界面
 function home() {
-    require(codePath + 'SrcJiexi.js');
-    homePage();
+    addListener("onClose", $.toString(() => {
+        clearMyVar('主页显示内容');
+    }));
+
+    let d = [];
+    d.push({
+        title: getMyVar('主页显示内容', '1')=="1"?`‘‘’’<b><span style="color: `+Color+`">解析列表</span></b>`:'解析列表',
+        url: $('#noLoading#').lazyRule(() => {
+            putMyVar('主页显示内容', '1');
+            refreshPage();
+            return "hiker://emtpy";
+        }),
+        img: 'http://123.56.105.145/tubiao/system/42.png',
+        col_type: "icon_3"
+    });
+    d.push({
+        title: getMyVar('主页显示内容', '1')=="2"?`‘‘’’<b><span style="color: `+Color+`">调用管理</span></b>`:'调用管理',
+        url: $('#noLoading#').lazyRule(() => {
+            putMyVar('主页显示内容', '2');
+            refreshPage();
+            return "hiker://emtpy";
+        }),
+        img: 'http://123.56.105.145/tubiao/system/42.png',
+        col_type: "icon_2"
+    });
+    d.push({
+        title: getMyVar('主页显示内容', '1')=="3"?`‘‘’’<b><span style="color: `+Color+`">解析设置</span></b>`:'解析设置',
+        url: $('#noLoading#').lazyRule(() => {
+            putMyVar('主页显示内容', '3');
+            refreshPage();
+            return "hiker://emtpy";
+        }),
+        img: 'http://123.56.105.145/tubiao/system/43.png',
+        col_type: "icon_3"
+    });
+    
+    if(getMyVar('主页显示内容', '1')=='1'){
+        require(jxCodePath + 'SrcJiexi.js');
+        jxItemPage(d);
+    }else if(getMyVar('主页显示内容', '1')=='2'){
+        require(jxCodePath + 'SrcCall.js');
+        jxCallPage(d);
+    }else{
+        jxSetPage(d);
+    }
 }
 // 解析列表
 function jxItem() {
-    require(codePath + 'SrcJiexi.js');
+    require(jxCodePath + 'SrcJiexi.js');
     jxItemPage();
 }
 // 解析列表
 function jxSet() {
-    require(codePath + 'SrcJiexi.js');
+    require(jxCodePath + 'SrcJiexi.js');
     jxSetPage();
 }
 // 调用解析
 function lazy(input, dataObj) {
-    require(codePath + 'SrcParse.js');
+    require(jxCodePath + 'SrcParse.js');
     return SrcParse(input, dataObj);
 }
 

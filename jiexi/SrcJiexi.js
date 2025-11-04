324a3dcf5143c80dc25ab7f81cb4110a078c58b4
src48597962 <48597962@qq.com>
2025-11-04 08:12:14 +0000

    更新 jiexi/SrcJiexi.js

--- jiexi/SrcJiexi.js
+++ jiexi/SrcJiexi.js
@@ -16,17 +16,17 @@ function homePage() {
             return "hiker://emtpy";
         }),
         img: 'http://123.56.105.145/tubiao/system/42.png',
-        col_type: "icon_2"
+        col_type: "icon_3"
     });
     d.push({
-        title: getMyVar('主页显示内容', '1')=="2"?`‘‘’’<b><span style="color: `+Color+`">解析设置</span></b>`:'解析设置',
+        title: getMyVar('主页显示内容', '1')=="3"?`‘‘’’<b><span style="color: `+Color+`">解析设置</span></b>`:'解析设置',
         url: $('#noLoading#').lazyRule(() => {
-            putMyVar('主页显示内容', '2');
+            putMyVar('主页显示内容', '3');
             refreshPage();
             return "hiker://emtpy";
         }),
         img: 'http://123.56.105.145/tubiao/system/43.png',
-        col_type: "icon_2"
+        col_type: "icon_3"
     });
     
     if(getMyVar('主页显示内容', '1')=='1'){

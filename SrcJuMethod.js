152e037da688da98ff472ffee26404e617f1c1d3
src48597962 <48597962@qq.com>
2025-11-21 00:07:02 +0000

    更新 SrcJuMethod.js

--- SrcJuMethod.js
+++ SrcJuMethod.js
@@ -1271,9 +1271,17 @@ function banner(start, arr, data, cfg){
 // x5中转聚阅二级
 function x5toerji(jkdata, extra, MY_RULE) {
     MY_RULE = MY_RULE || JSON.parse(fetch("hiker://home@聚阅"));
+    jkdata = jkdata || storage0.getMyVar('一级源接口信息');
+    extra = extra || getVar('轮播数据') ||{};
     extra.data = jkdata;
     extra.name = extra.title || extra.name || extra.pageTitle;
+    clearVar('轮播数据');
     return $.toString((MY_RULE, jkdata, extra) => {
+        if(typeof window.item != 'undefined'){
+            extra.name = window.item.title;
+            extra.img = window.item.img;
+            extra.url = window.item.url;
+        }
         fba.open(JSON.stringify({
             rule: "聚阅",
             title: extra.name || '详情',

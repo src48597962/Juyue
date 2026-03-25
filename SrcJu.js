fb731e666f4936391bf01d99bf33d447503dfe5f
src48597962 <48597962@qq.com>
2026-03-25 14:24:57 +0000

    更新 SrcJu.js

--- SrcJu.js
+++ SrcJu.js
@@ -1672,9 +1672,7 @@ function sousuo() {
         extra: {
             delegateOnlySearch: true,
             rules: $.toString((name) => {
-                xlog(name);
-                xlog(input);
-
+                xlog(MY_URL);
                 let info = storage0.getMyVar('一级源接口信息') || {};
                 let keyword = name.split('  ')[0].trim();
                 let keyword2;

321bc8e41954a1c46d4b27d5ba8e502094a5d0ed
src48597962 <48597962@qq.com>
2026-03-28 06:51:49 +0000

    更新 SrcJu.js

--- SrcJu.js
+++ SrcJu.js
@@ -12,7 +12,7 @@ function yiji(testSource) {
         try{
             programversion = $.require("config").version || MY_RULE.version || 0;
         }catch(e){}
-        if(programversion<8){
+        if(programversion<9){
             confirm({
                 title: "温馨提示",
                 content: "发现小程序新版本",

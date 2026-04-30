471433bb1f096db88815e764847574fadf204392
src48597962 <48597962@qq.com>
2026-04-30 06:59:31 +0000

    添加 SrcJuDev.js

---
+++ SrcJuDev.js
@@ -0,0 +1,21 @@
+let d = [];
+d.push({
+    title: '分析',
+    desc: '输入网站url',
+    url: $.toString(() => {
+        input = input.trim();
+        if(!input){
+            return 'toast://输入不能为空';
+        }
+        
+        return 'hiker://empty';
+    }),
+    col_type: 'input',
+    extra: {
+        defaultValue: ''
+    }
+})
+
+
+
+setReust(d);

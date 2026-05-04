6ca3818f85daed20e64f151552e93e8e067fb72a
src48597962 <48597962@qq.com>
2026-05-04 14:09:45 +0000

    更新 SrcJuDev.js

--- SrcJuDev.js
+++ SrcJuDev.js
@@ -350,43 +350,45 @@ function autoGenerateLocationList(html) {
     return result;
 }
 
+if(html){
+    // 使用
+    let 定位列表 = autoGenerateLocationList(html);
+    log(JSON.stringify(定位列表, null, 2));
 
-// 使用
-let 定位列表 = autoGenerateLocationList(html);
-log(JSON.stringify(定位列表, null, 2));
+    //log('html源码>'+html);
+    /*
+    var 定位列表 = [{
+        一级分类: 'body&&.stui-header__menu||.hl-nav',
+        子分类: 'body&&li:not(:matches(首页|资讯|专题|短视频|APP下载|音乐|留言|最新|排行))'//:gt(0)
+    },{
+        一级分类: 'body&&.stui-screen__list||.hl-filter-wrap:not(:matches(字母))',
+        子分类: 'body&&li:has(a:not(:empty)):lt(12)'//:gt(0)
+    },{
+        一级分类: 'body&&.hl-rb-title',
+        子分类: 'body&&a'
+    }]
+    */
 
-//log('html源码>'+html);
-/*
-var 定位列表 = [{
-    一级分类: 'body&&.stui-header__menu||.hl-nav',
-    子分类: 'body&&li:not(:matches(首页|资讯|专题|短视频|APP下载|音乐|留言|最新|排行))'//:gt(0)
-},{
-    一级分类: 'body&&.stui-screen__list||.hl-filter-wrap:not(:matches(字母))',
-    子分类: 'body&&li:has(a:not(:empty)):lt(12)'//:gt(0)
-},{
-    一级分类: 'body&&.hl-rb-title',
-    子分类: 'body&&a'
-}]
-*/
+    // '0' 为默认不折叠，'1' 为默认折叠
+    let 当前折叠状态 = getMyVar('header.fold', '1')
 
-// '0' 为默认不折叠，'1' 为默认折叠
-let 当前折叠状态 = getMyVar('header.fold', '1')
+    // 引入动态分类依赖
+    // 框架已经稳定，使用 require 更佳
+    let htmlCategories = require(config.聚阅.match(/http(s)?:\/\/.*\//)[0] + 'plugins/categories-header.js')
+    htmlCategories.界面(d)
+        .分类链接(true_url)
+        .源码(html)
+        .页码(page)
+        .添加分类定位(定位列表)
+        .开启内置折叠功能() // 必须
+        .折叠按钮样式({
+            title: 当前折叠状态 == "1" ? "‘‘️▼’’" : "‘‘▲’’"
+        }) // 可选
+        .折叠(当前折叠状态) // 必须
+        .选中的分类颜色(分类颜色)
+        .开始打造分类();
 
-// 引入动态分类依赖
-// 框架已经稳定，使用 require 更佳
-let htmlCategories = require(config.聚阅.match(/http(s)?:\/\/.*\//)[0] + 'plugins/categories-header.js')
-htmlCategories.界面(d)
-    .分类链接(true_url)
-    .源码(html)
-    .页码(page)
-    .添加分类定位(定位列表)
-    .开启内置折叠功能() // 必须
-    .折叠按钮样式({
-        title: 当前折叠状态 == "1" ? "‘‘️▼’’" : "‘‘▲’’"
-    }) // 可选
-    .折叠(当前折叠状态) // 必须
-    .选中的分类颜色(分类颜色)
-    .开始打造分类();
+}
 
 
 setResult(d);

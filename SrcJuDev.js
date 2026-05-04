b28af176748c942d3dacf5e14c4c1071ecb88a12
src48597962 <48597962@qq.com>
2026-05-04 07:42:02 +0000

    更新 SrcJuDev.js

--- SrcJuDev.js
+++ SrcJuDev.js
@@ -23,38 +23,23 @@ d.push({
 var 分类颜色 = '#3399cc';
 var page = MY_PAGE;
 var true_url = getMyVar('header.url', MY_URL);
-log('请求地址>'+true_url);
-var html = fetch(true_url);
-
 let 链接处理工具 = require(config.聚阅.match(/http(s)?:\/\/.*\//)[0] + 'plugins/UrlProcessor.js')
 true_url = 链接处理工具
     .链接(true_url)
     .页码(page)
     .获取处理结果();
 MY_URL = true_url;
-
+log('请求地址>'+MY_URL);
+var html = fetch(MY_URL);
 
 //log('html源码>'+html);
-// 临时调试代码
-// 在 build() 方法中添加
-let uls = parseDomForArray(html, 'body&&.stui-screen__list');
-log("找到的ul数量: " + uls.length);
-log(uls);
-for(let i = 0; i < uls.length; i++) {
-    log(`第${i+1}个ul的HTML前200字符:`, uls[i].substring(0, 200));
-    let lis = parseDomForArray(uls[i], 'li');
-    log(`  包含${lis.length}个li`);
-    for(let j = 0; j < Math.min(lis.length, 3); j++) {
-        log(`    第${j+1}个li:`, lis[j].substring(0, 100));
-    }
-}
 
 var 定位列表 = ([{
     一级分类: 'body&&.stui-header__menu',
     子分类: 'body&&li:not(:matches(首页|资讯|专题|短视频|APP下载))',//:gt(0)
 },{
     一级分类: 'body&&.stui-screen__list:not(:matches(字母))',
-    子分类: 'body&&li:has(a:not(:empty)):lt(12):gt(0)',//:gt(0)
+    子分类: 'li:has(a:not(:empty)):lt(12):gt(0)',//:gt(0)
 }])
 
 

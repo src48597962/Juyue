69925de720de882f3b32ec4e8306fb0dd26273c7
src48597962 <48597962@qq.com>
2026-06-11 13:35:14 +0000

    更新 SrcJu.js

--- SrcJu.js
+++ SrcJu.js
@@ -912,7 +912,7 @@ function erji() {
                         if(input=='扩展搜索'){
                             return $.expandSearch;
                         }else if(input.includes('收藏')){
-                            return 'hiker://emtpy@lazyRule=.js:' + $.addCaseObj;
+                            return '#noLoading#@lazyRule=.js:' + $.addCaseObj;
                         }
                     }),
                     pic_url: getIcon(erIcons[4].img, false, erIcons[4].color),

a211508b5d5047ccbbfff8c88cc3d160b4583a33
src48597962 <48597962@qq.com>
2026-07-03 13:42:13 +0000

    更新 SrcJu.js

--- SrcJu.js
+++ SrcJu.js
@@ -158,7 +158,7 @@ function yiji(testSource) {
                 const hikerPop = $.require(libspath + 'plugins/hikerPop.js');
                 let pop = hikerPop.selectBottom({
                     options: ["搜索框搜索方式", "搜索框显示建议词", "显示搜索历史数量","--------","三针短剧","聚影直播"],
-                    columns: 1,
+                    columns: 3,
                     height: 0.5,
                     noAutoDismiss: true,
                     click(a) {
